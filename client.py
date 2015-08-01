eaafc33badbe6fe4bf9eebd423a93537892c7560
Gregory Ostermayr & Patrick Allen <gregory.ostermayr@gmail.com>
2015-08-01 00:16:01 -0400

    added input from command line

--- client.py
+++ client.py
@@ -1,14 +1,17 @@
 import socket
 import sys
 
-target_host = "www.google.com"
-target_port = 80
+host = raw_input("input host ")
+port = input("input port ")
 
 client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-client.connect((target_host, target_port))
-client.send("GET / HTTP/1.1\r\nHost: www.google.com\r\n\r\n")
+print "debug",host, port
+client.connect((host, port))
+client.send("GET / HTTP/1.1\r\nHost:{}\r\n\r\n".format(host))
 response = client.recv(4096)
+
+# if 301 then follow up with has moved to....
 print response
 
 
